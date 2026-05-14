3cb7349827a8169cab1fb98eed19333d6f2e0d15
Luiz Seidl <luizseidl@gmail.com>
2026-05-13 22:46:59 -0300

    final aula do dia

---
+++ 02-semestre/paradigmas-ling-prog-python/pythonbasico.py
@@ -0,0 +1,13 @@
+nome = input("Digite seu nome: ")
+dia = input("Digite o dia do seu nascimento: ")
+mes = input("Digite o mês do seu nascimento: ")
+ano = input("Digite o ano do seu nascimento: ")
+
+print("Olá,", nome, "! Prazer em te conhecer!")
+print("Você nasceu no dia", dia, "de", mes, "de", ano, ".")
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+sum = num1 + num2
+
+print("A soma é", sum)
